1b77167f51788f51052389039847a9f3eb4cb75e
navneetchandra34-dotcom <navneetchandra34@gmail.com>
2026-03-19 14:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const sections = document.querySelectorAll(".section");
-
-window.addEventListener("scroll", () => {
-
-sections.forEach(sec=>{
-const top = sec.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-sec.style.opacity="1";
-sec.style.transform="translateY(0)";
-}
-
-});
-
-});
+console.log("Website Loaded");
